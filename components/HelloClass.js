7da231567b8fb7a16fcb2d86d7c7f36e30d9268d
Adam <adamnugat@gmail.com>
2018-07-24 12:31:36 +0200

    html planszy do gry, style, obrazki #1

--- components/HelloClass.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export class HelloPlanet extends React.Component {
-	render() {
-		return <h1>siemasz</h1>
-	}
-}
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Chinczyk} from './components/main';
 
 ReactDOM.render(
-	<h1>hello world</h1>,
-	document.getElementById('firstapp')
+	<Chinczyk />,
+	document.getElementById('chinczykapp')
 );
